f2a8b0f8553bc4c8212398546b513fa57e98b26d
Chandan Kumar Sahu <sahuchandankumar65@gmail.com>
2024-10-09 20:41:29 +0530

    file added

---
+++ DailyJavaPractice/JavaLearningJourney/src/com/chandan/practice/oct10/Narrownig.java
@@ -0,0 +1,13 @@
+package com.chandan.practice.oct10;
+
+public class Narrownig {
+public static void main(String[] args) {
+	int n = 129;
+	byte b =(byte)n;
+	System.out.println(n);
+	System.out.println(b);
+
+}
+}
+
+//The Process oF converting Higher Range Type Data Into Lower Range type Data is called Narrowing
